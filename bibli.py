2f077cc7877b761a1f6cc2e8e16e290a7588a589
AMGMounir <153465738+AMGMounir@users.noreply.github.com>
2024-04-03 16:09:45 +0200

    Add files via upload

--- bibli.py
+++ bibli.py
@@ -24,13 +24,16 @@ def get_Dependencies(task_system, tache):
 
 def runSeq(task_system):
     tache_exec = set()
-
     def exec_tache(task):
         if task.run:
             task.run()
+            global start 
+            start = time.perf_counter()
         tache_exec.add(task.name)
         print("La tache ", task.name,"vient d'etre executé.")
         time.sleep(1)
+        global end
+        end = time.perf_counter()
 
     while len(tache_exec) < len(task_system):
         for task, dependencies in task_system.items():
@@ -38,12 +41,34 @@ def runSeq(task_system):
                 if dependencies:
                     print("La tache ", task.name, "est dependante de:", get_Dependencies(task_system, task))
                 exec_tache(task)
+    print ("temps d'execution", end-start)
 
 
 
+def run(task_system):
+    tache_exec = set()
+    threads = []
 
+    def exec_tache(task):
+        if task.run:
+            task.run()
+        tache_exec.add(task.name)
 
-
+    while len(tache_exec) < len(task_system):
+        for task, dependencies in task_system.items():
+            if task.name not in tache_exec and all(dep.name in tache_exec for dep in dependencies):
+                if dependencies:
+                    time.sleep(1)
+                    print("La tache", task.name, "est dépendante de:", ", ".join(get_Dependencies(task_system, task)))
+                thread = threading.Thread(target=exec_tache, args=(task,))
+                threads.append(thread)
+                thread.start()
+                start = time.perf_counter()
+                print("La tache", task.name, "vient d'etre executée.")
+    for thread in threads:
+        thread.join()
+        end = time.perf_counter()
+    print ("temps d'execution", end-start)
 
 
 def errormessage(task_system):
--- test.py
+++ test.py
@@ -3,6 +3,12 @@ from bibli import *
 X = 0
 Y = 0 
 Z = 0 
+W = 0
+A = 0
+B = 0
+C = 0
+D = 0
+
 
 def runT1():
     global X
@@ -12,31 +18,64 @@ def runT2():
     global Y
     Y = 2
 
-def runTsomme():
-    global X, Y, Z
-    Z = X + Y
+def runT3():
+    global Z
+    Z = 3
+
+def runT4():
+    global W
+    W = 3
+
+def runT5():
+    global A
+    A = 3
+
+def runT6():
+    global B
+    B = 3
+
+def runT7():
+    global C
+    C = 3
+
+def runT8():
+    global D
+    D = 3
+
 
 t1 = Task(name="T1", run=runT1, writes=[X])
 t2 = Task(name="T2", run=runT2, writes=[Y])
-tSomme = Task(name="somme", run=runTsomme, reads=["X", "Y"], writes=[Z])
+t3 = Task(name="t3", run=runT3, reads=["X", "Y"], writes=[Z])
+t4 = Task(name="T4", run=runT4, writes=[W])
+t5 = Task(name="T5", run=runT5, writes=[A])
+t6 = Task(name="T6", run=runT6, reads=["W", "A"], writes=[B])
+t7 = Task(name="T7", run=runT7, writes=[C])
+t8 = Task(name="T8", run=runT8, writes=[D])
 
 task_system = {
     Task("T1", run=runT1): [],
-    Task("somme", run=runTsomme): [Task("T1"), Task("T2")],
-    Task("T2", run=runT2): [Task("T1")]
+    Task("T3", run=runT3): [Task("T2")],
+    Task("T2", run=runT2): [Task("T1")],
+    Task("T5", run=runT5): [Task("T3"),Task("T4")],
+    Task("T7", run=runT7): [Task("T5"), Task("T6")],
+    Task("T8", run=runT8): [Task("T7")],
+    Task("T6", run=runT6): [Task("T4")],
+    Task("T4", run=runT4): [Task("T2")]
 }
 
 # Get dependencies for a specific task
-tache = Task("somme", run=runTsomme)
+tache = Task("T5", run=runT5)
 dependencies = get_Dependencies(task_system, tache)
-print(dependencies)
+print("Les dependances de ", t5.name," sont : ",dependencies)
 
 
 #runSeq
+print("\nexecution sequentielle :\n")
 runSeq(task_system)
 
 
 #run
-#run(task_system)
+print("\nexecution parallele :\n")
+run(task_system)
 
 errormessage(task_system)
