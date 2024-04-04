483f5e5c6812d4d303f720f8fb10d0e4b3d77cbc
kaoutar02 <122029758+kaoutar02@users.noreply.github.com>
2024-04-04 13:02:20 +0200

    Update bibli.py

--- bibli.py
+++ bibli.py
@@ -1,5 +1,8 @@
 import time
 import threading
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Task:
     def __init__(self, name="", reads=None, writes=None, run=None):
         self.name = name
@@ -11,7 +14,7 @@ class TaskSystem:
     def __init__(self, task_system=[], dependencies={}):
         self.task_system = task_system or []
         self.dependencies = dependencies or {}
-
+        Verification(self.task_system, self.dependencies)
 
 
 def get_Dependencies(task_system, tache):
@@ -69,8 +72,41 @@ def run(task_system):
         thread.join()
         end = time.perf_counter()
     print ("temps d'execution", end-start)
-
-
+    
+def Verification(task_system, dependencies):
+    # Verification des noms de taches dupliqués
+    task_names = [task.name for task in task_system]
+    if len(task_names) != len(set(task_names)):
+        raise ValueError("Erreur : Des noms de taches sont dupliques.")
+    
+    # Verification des taches inexistantes dans les dependances
+    all_task_names = set(task.name for task in task_system)
+    for task, dependency_list in dependencies.items():
+        if task.name not in all_task_names:
+            raise ValueError(f"Erreur : Tâche inexistante dans le système de tâches : {task}.")
+        print(dependency_list)
+        for dependency in dependency_list:
+            if dependency.name not in all_task_names:
+                raise ValueError(f"Erreur : Tâche inexistante dans les dépendances de la tâche {task} : {dependency}.")
+    
+    # Vérification de la cohérence des dépendances
+    for task, dependency_list in dependencies.items():
+        for dependency in dependency_list:
+            if task == dependency:
+                raise ValueError(f"Erreur : La tâche {task} ne peut pas dépendre d'elle-même.")
+            if task in dependencies[dependency]:
+                raise ValueError(f"Erreur : Il y a une boucle de dépendance entre {task} et {dependency}.")
+    
+    return True
+def draw(task_system):
+    G = nx.DiGraph()
+    
+    for task, dependency_list in task_system.items():
+        for dependency in dependency_list:
+            G.add_edge(dependency, task)
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True)
+    plt.show()
 def errormessage(task_system):
     if len(task_system) != len(set(task_system)):
         print("duplicate found")
